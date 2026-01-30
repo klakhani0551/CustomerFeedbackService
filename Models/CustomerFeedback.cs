06939ebdcdd5b8a02ee760cd8fa5dc0a163e4d24
klakhani0551 <klakhani0551@conestogac.on.ca>
2026-01-30 15:25:05 -0500

    Created Folders and added basic Model code  file

---
+++ Models/CustomerFeedback.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CustomerFeedbackService.Models
+{
+    public class CustomerFeedback
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string CustomerName { get; set; }
+
+        [Range(1, 5)]
+        public double Rating { get; set; }
+
+        [StringLength(500)]
+        public string FeedbackComment { get; set; }
+
+        public DateTime SubmittedAt { get; set; } = DateTime.Now;
+    }
+}
